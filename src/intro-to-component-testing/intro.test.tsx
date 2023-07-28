edbdd8ca2d801ee56cceffbb34c167bac9b89af5
Mawin <kobori1590@gmail.com>
2023-07-28 14:35:06 +0700

    setup jest

--- src/intro-to-component-testing/intro.test.tsx
+++ src/intro-to-component-testing/intro.test.tsx
@@ -1,17 +1,16 @@
 import { act, fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
-import { vi } from 'vitest';
 
 describe("App Component", () => {
     it("should render react", () => {
         render(<App/>)
-        vi.spyOn(global.Math, 'floor').mockReturnValue(0);
+        jest.spyOn(Math, 'random').mockReturnValue(0);
         const increaseButton = screen.getByText(/Random Button/)
         act(() => {
             fireEvent.click(increaseButton)
             increaseButton.click()
         })
         screen.debug()
-        expect(screen.queryByText(/Random name : Maria Anders/)).toBeInTheDocument()
+        expect(screen.getByText(/Random name : Maria Anders/)).toBeInTheDocument()
     })
 })
